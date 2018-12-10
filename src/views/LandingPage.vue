061a597877f46f6b77022819c7a39f2a0e6b24b3
teddy <theodorelrose@gmail.com>
2018-12-10 15:03:39 +0100

    Added basic landing page, and loading spin wheels to standings and matchups pages

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,10 @@
+<template>
+ <v-content class="chat">
+        <v-container>
+            <v-card>
+                <h1 class="display-5"> Here is a landing page </h1>
+                 <v-btn class="cyan--text" outline round to="/standings">Standings</v-btn>
+            </v-card>
+        </v-container>
+    </v-content>
+</template>
